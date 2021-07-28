03e20b82327e2393805786ff0c54ac857da89d15
sofiarnunes <sofia.ri.nunes@gmail.com>
2021-07-29 00:24:16 +0100

    Update final

---
+++ js/arrow_click.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#arrow").click(function (){
+        $("#prize").show();
+        $('html, body').animate({
+            scrollTop: $("#prize").offset().top
+        }, 1000);
+        e.preventDefault();
+    }); 
+})
