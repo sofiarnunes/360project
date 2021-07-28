68cec30f2c788388e643750ca302ae4266850078
catarinasjpereira90 <catarinasjpereira90@gmail.com>
2021-07-28 23:47:41 +0100

    Cat_commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 $(document).ready(function () {
     $("#submit").click(function (e) {
         var inputCode = $("#code").val();
-        if (inputCode != "A1A1") {
-            $(".gifts").hide();
-        } else {
-            $(".gifts").show();
+        if (inputCode == "AAAA") {
+            $(".announcement").show();
+            $('html, body').animate({
+                scrollTop: $(".announcement").offset().top
+            }, 1000);
+        } else if (inputCode == "BBBB") {
+            $("#win-20").attr("src", 'https://github.com/sofiarnunes/Halls-360Project/blob/main/img/50_Group.png?raw=true');
+            $(".announcement").show();
+            $('html, body').animate({
+                scrollTop: $(".announcement").offset().top
+            }, 1000);
+        } else if (inputCode == "CCCC") {
+            $("#win-20").attr("src", 'https://github.com/sofiarnunes/Halls-360Project/blob/main/img/100_Group.png?raw=true');
+            $(".announcement").show();
+            $('html, body').animate({
+                scrollTop: $(".announcement").offset().top
+            }, 1000);
+        } else if (inputCode == "DDDD") {
+            $("#win-20").attr("src", 'https://github.com/sofiarnunes/Halls-360Project/blob/main/img/250_Group.png?raw=true');
+            $(".announcement").show();
+            $('html, body').animate({
+                scrollTop: $(".announcement").offset().top
+            }, 1000);
         }
         e.preventDefault();
-    });
-});
+    }); 
+})
+
+
+
--- js/new.js
+++ js/new.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
-    $("#submit").click(function (e) {
-        var inputCode = $("#code").val();
-        if (inputCode != "A1A1") {
-            $(".prize").hide();
-        } else {
-            $(".prize").show();
-            $('html, body').animate({
-                scrollTop: $(".prize").offset().top}, 1000);
-        };
-        e.preventDefault();
-    });
-});
+// $(document).ready(function () {
+//     $("#submit").click(function (e) {
+//         var inputCode = $("#code").val();
+//         if (inputCode != "A1A1") {
+//             $(".prize").hide();
+//         } else {
+//             $(".prize").show();
+//             $('html, body').animate({
+//                 scrollTop: $(".prize").offset().top}, 1000);
+//         };
+//         e.preventDefault();
+//     });
+// });
