69af74f64b23522276b5ee6d70c88e8fc92e0dbe
catarinasjpereira90 <catarinasjpereira90@gmail.com>
2021-07-29 12:46:50 +0100

    12:46_commit_cat

--- js/main.js
+++ js/main.js
@@ -2,23 +2,26 @@ $(document).ready(function () {
     $("#submit").click(function (e) {
         var inputCode = $("#code").val();
         if (inputCode == "AAAA") {
+            $(".hero").hide();
             $(".announcement").show();
             $('html, body').animate({
                 scrollTop: $(".announcement").offset().top
             }, 1000);
         } else if (inputCode == "BBBB") {
+            $(".hero").hide();
             $("#win-20").attr("src", 'https://github.com/sofiarnunes/Halls-360Project/blob/main/img/50_Group.png?raw=true');
             $(".announcement").show();
             $('html, body').animate({
                 scrollTop: $(".announcement").offset().top
             }, 1000);
         } else if (inputCode == "CCCC") {
+            $(".hero").hide();
             $("#win-20").attr("src", 'https://github.com/sofiarnunes/Halls-360Project/blob/main/img/100_Group.png?raw=true');
             $(".announcement").show();
             $('html, body').animate({
                 scrollTop: $(".announcement").offset().top
             }, 1000);
-        } else if (inputCode == "DDDD") {
+        } else if (inputCode == "DDDD") {$(".hero").hide();
             $("#win-20").attr("src", 'https://github.com/sofiarnunes/Halls-360Project/blob/main/img/250_Group.png?raw=true');
             $(".announcement").show();
             $('html, body').animate({
