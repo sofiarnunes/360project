28de09b251c96beb5b3ae2632281ffd283cb96df
sofiarnunes <sofia.ri.nunes@gmail.com>
2021-07-29 14:31:49 +0100

    Add login js Sof

---
+++ js/login-show.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#btn-cravas").click(function (e) {
+        $("#login").show();
+        $("#login").css("background-color", "rgba(0,0,0,0.5)");
+        e.preventDefault();
+    }); 
+    
+})
