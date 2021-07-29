5334a463c269f1ed79672dec0422db6350bdde5d
sofiarnunes <sofia.ri.nunes@gmail.com>
2021-07-29 14:53:06 +0100

    Update js Sof

--- js/login-show.js
+++ js/login-show.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
         $("#login").css("background-color", "rgba(0,0,0,0.5)");
         e.preventDefault();
     }); 
-    $("#image-cravas").click(function (e) {
-        $("#login").show();
-        $("#login").css("background-color", "rgba(0,0,0,0.5)");
-        e.preventDefault();
-    }); 
+    // $("#image-cravas").click(function (e) {
+    //     $("#login").show();
+    //     $("#login").css("background-color", "rgba(0,0,0,0.5)");
+    //     e.preventDefault();
+    // }); 
 })
