73e5ca30e69152195657f23a77ef95e843dc53e4
sofiarnunes <sofia.ri.nunes@gmail.com>
2021-08-04 16:52:55 +0100

    Last Update SOF

--- js/arrow_click.js
+++ js/arrow_click.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
-     $("#arrow").click(function (){
-         $('html, body').animate({
-             scrollTop: $("#prize").offset().top
-         }, 1000);
-         e.preventDefault();
-     }); 
-})
+// $(document).ready(function () {
+//      $("#arrow").click(function (){
+//          $('html, body').animate({
+//              scrollTop: $("#prize").offset().top
+//          }, 1000);
+//          e.preventDefault();
+//      }); 
+// })
--- js/main.js
+++ js/main.js
@@ -35,5 +35,32 @@ $(document).ready(function () {
         }
         e.preventDefault();
     }); 
+
+    $("#code").keyup(function(){
+        $(this).val($(this).val().toUpperCase());
+    })
+
+    $("#arrow").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#prize").offset().top
+        }, 1000);
+        e.preventDefault();
+    }); 
     
+    $("#btn-cravas").click(function (e) {
+        $("#prize").css("position", "fixed");
+        $("#prize").css("z-index", "10");
+        $("#prize").css("right", "0");
+        $("#prize").css("bottom", "0");
+        $("#login").show();
+        $("#login").css("background-color", "rgba(0,0,0,0.5)");
+
+        e.preventDefault();
+    }); 
+    $("#exit").click(function (e) {
+        $("#prize").css("position", "relative");
+        $("#prize").css("z-index", "0");
+        $("#login").hide();
+        e.preventDefault();
+    }); 
 })
